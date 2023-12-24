a81c12347e8bbef900dd71d55fd275909a422baf
EduardoMoreaes <17eduardo05@gmail.com>
2023-12-24 16:20:40 -0300

    reorganized

--- cobra/1imports.py
+++
@@ -1,8 +0,0 @@
-# Biblioteca para criar a janela
-from tkinter import *
-
-# Módulo da biblioteca que cria uma janela de alerta
-from tkinter.messagebox import showinfo
-
-# Biblioteca para gerar números aleatórios
-import random
